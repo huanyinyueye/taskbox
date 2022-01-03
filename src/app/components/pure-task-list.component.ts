f6637b6ba75683b48a31bc4b65defb7feefbb4ed
huanyinyueye <lightfugitive@gmail.com>
2022-01-04 07:58:58 +0800

    data

---
+++ src/app/components/pure-task-list.component.ts
@@ -0,0 +1,10 @@
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Task } from '../models/task.model';
+@Component({
+  selector: 'app-pure-task-list',
+  // same content as before with the task-list.component.ts
+})
+
+export class PureTaskListComponent {
+  // same content as before with the task-list.component.ts
+}
