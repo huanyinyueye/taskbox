1da355f00999a2f519b49777220e15cda46aa60a
huanyinyueye <lightfugitive@gmail.com>
2022-01-04 08:10:35 +0800

    data

--- src/app/components/pure-task-list.component.ts
+++ src/app/components/pure-task-list.component.ts
@@ -1,10 +1,36 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Select, Store } from '@ngxs/store';
+import { TasksState, ArchiveTask, PinTask } from '../state/task.state';
 import { Task } from '../models/task.model';
+import { Observable } from 'rxjs';
+
 @Component({
   selector: 'app-pure-task-list',
-  // same content as before with the task-list.component.ts
+  template: `
+    <app-pure-task-list
+      [tasks]="tasks$ | async"
+      (onArchiveTask)="archiveTask($event)"
+      (onPinTask)="pinTask($event)"
+    ></app-pure-task-list>
+  `,
 })
 
 export class PureTaskListComponent {
-  // same content as before with the task-list.component.ts
+  @Select(TasksState.getAllTasks) tasks$: Observable<Task[]>;
+
+  constructor(private store: Store) {}
+
+  /**
+   * Component method to trigger the archiveTask event
+   */
+  archiveTask(id: string) {
+    this.store.dispatch(new ArchiveTask(id));
+  }
+
+  /**
+   * Component method to trigger the pinTask event
+   */
+  pinTask(id: string) {
+    this.store.dispatch(new PinTask(id));
+  }
 }
